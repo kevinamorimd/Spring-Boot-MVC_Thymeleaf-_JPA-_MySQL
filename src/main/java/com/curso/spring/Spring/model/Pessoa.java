f7a17893da62152ee186fc9a393c2ae9d3e59d6c
KevinAmorimD <kevin.amorim02@gmail.com>
2022-12-19 22:56:32 -0300

    criacao classe pessoa

---
+++ src/main/java/com/curso/spring/Spring/model/Pessoa.java
@@ -0,0 +1,40 @@
+package com.curso.spring.Spring.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pessoa implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	private String nome;
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	private String sobrenome;
+}
