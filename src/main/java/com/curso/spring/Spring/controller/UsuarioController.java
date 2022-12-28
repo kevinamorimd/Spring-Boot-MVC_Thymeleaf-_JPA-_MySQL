e1704f9dca1573eb71b4d846cf5ee86e7068441b
KevinAmorimD <kevin.amorim02@gmail.com>
2022-12-27 23:09:40 -0300

    Post USuario

--- src/main/java/com/curso/spring/Spring/controller/UsuarioController.java
+++ src/main/java/com/curso/spring/Spring/controller/UsuarioController.java
@@ -5,10 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +37,14 @@ public class UsuarioController {
 	
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		return new ResponseEntity<List<Usuario>> (list,HttpStatus.OK);
+		
+	}
+	
+	@PostMapping(value="/")
+	public ResponseEntity<Usuario> cadastrar (@RequestBody Usuario usuario ) {
+	
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario> (usuarioSalvo,HttpStatus.OK);
 		
 	}
 }
