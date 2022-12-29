b4bc8710a40c5005ba2a23421bed03cb8f61b302
KevinAmorimD <kevin.amorim02@gmail.com>
2022-12-28 22:53:50 -0300

    Delete Usuario

--- src/main/java/com/curso/spring/Spring/controller/UsuarioController.java
+++ src/main/java/com/curso/spring/Spring/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,12 @@ public class UsuarioController {
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario> (usuarioSalvo,HttpStatus.OK);
 		
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public String deleteUsuario (@PathVariable (value="id") Long id) {
+		usuarioRepository.deleteById(id);
+		
+		return ("Ok");
 	}
 }
