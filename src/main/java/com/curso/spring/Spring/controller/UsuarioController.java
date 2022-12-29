9d056de30de27c9fe83392a09eebb333a3d08161
KevinAmorimD <kevin.amorim02@gmail.com>
2022-12-28 22:34:14 -0300

    Put Usuario

--- src/main/java/com/curso/spring/Spring/controller/UsuarioController.java
+++ src/main/java/com/curso/spring/Spring/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,14 @@ public class UsuarioController {
 	
 	@PostMapping(value="/")
 	public ResponseEntity<Usuario> cadastrar (@RequestBody Usuario usuario ) {
+	
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario> (usuarioSalvo,HttpStatus.OK);
+		
+	}
+	
+	@PutMapping(value="/")
+	public ResponseEntity<Usuario> alterarUsuario (@RequestBody Usuario usuario ) {
 	
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario> (usuarioSalvo,HttpStatus.OK);
